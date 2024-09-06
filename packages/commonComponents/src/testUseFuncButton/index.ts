4430a7907cc534fac8ecae8b1f5995e6e3422056
chenjie <chenjie@chenjiedeMacBook-Pro.local>
2024-09-06 15:55:12 +0800

    style： 格式化文档

--- packages/commonComponents/src/testUseFuncButton/index.ts
+++ packages/commonComponents/src/testUseFuncButton/index.ts
@@ -1,37 +1,46 @@
 /**
  * 函数式组件
  */
-import { type FunctionalComponent, ref, h } from "vue";
+import { type FunctionalComponent, ref, h } from 'vue'
 
 type FComponentProps = {
-    testName?: string
+  testName?: string
 }
 
 type Events = {
-    sendMessage: (count: number) => void
+  sendMessage: (count: number) => void
 }
 
 export const useFuncButton = () => {
+  const count = ref(0)
+  const updateCount = () => {
+    count.value++
+  }
 
-    const count = ref(0)
-    const updateCount = () => {
-        count.value++
-    }
+  const TextComp: FunctionalComponent = () => {
+    return h('div', { style: { 'font-weight': 'bold' } }, count.value)
+  }
 
-    const TextComp: FunctionalComponent = () => {
-        return h('div', {style: {"font-weight": "bold"} }, count.value)
-    }
+  const ButtonComp: FunctionalComponent<FComponentProps, Events> = (_props, { emit, slots }) => {
+    return h(
+      'div',
+      null,
+      h(
+        'button',
+        {
+          onClick: () => {
+            updateCount()
+            emit('sendMessage', count.value)
+          }
+        },
+        slots.default?.()
+      )
+    )
+  }
 
-    const ButtonComp: FunctionalComponent<FComponentProps, Events> = (_props, { emit, slots }) => {
-        return h("div", null, h("button", {
-                onClick: () => {updateCount(); emit('sendMessage', count.value)}
-            }, slots.default?.())
-        )
-    }
+  // 如果不这样写，就不会处理驼峰和短斜线之间的写法转换
+  ButtonComp.props = ['testName']
+  ButtonComp.emits = ['sendMessage']
 
-    // 如果不这样写，就不会处理驼峰和短斜线之间的写法转换
-    ButtonComp.props = ["testName"]
-    ButtonComp.emits = ["sendMessage"]
-
-    return [TextComp, ButtonComp]
+  return [TextComp, ButtonComp]
 }
